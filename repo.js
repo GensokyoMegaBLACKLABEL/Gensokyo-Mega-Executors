bc268433d97e1ed520e1eb6b55a100e44c0593c1
GensokyoMegaBLACKLABEL <nmlgc@nmlgc.net>
2021-01-10 17:50:12 +0700

    Repo.js have been finshed!
    While Patches please wait this may take a few moments!

--- repo.js
+++ repo.js
@@ -1,5 +1,9 @@
 {
 	"contact": "AutoThaiLand#6152",
 	"id": "Gensokyo-Mega-Executors",
+	"patches": {},
+	"servers": [
+		"https://raw.githubusercontent.com/GensokyoMegaBLACKLABEL/Gensokyo-Mega-Executors/master/"
+	],
 	"title": "Gensokyo Super Mega Black Label"
 }
