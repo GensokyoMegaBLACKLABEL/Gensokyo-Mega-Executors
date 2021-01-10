9dc5bbbb66eb762ec280f8c6cbe9e0a98fa4edc2
GensokyoMegaBLACKLABEL <nmlgc@nmlgc.net>
2021-01-10 17:54:49 +0700

    yay

---
+++ THSMBL_01/patch.js
@@ -0,0 +1,13 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin",
+		"nmlgc/western_name_order"
+	],
+	"id": "THBL_01",
+	"servers": [
+		"https://mirrors.thpatch.net/Gensokyo.EXE/THBL_01/"
+	],
+	"title": "Touhou Black Label 01 - Desire.EXE",
+	"update": true
+}
