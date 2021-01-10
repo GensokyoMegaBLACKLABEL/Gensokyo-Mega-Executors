f60cca47658d0f619f2a2a0a3ad12a36fd1367fd
GensokyoMegaBLACKLABEL <75157503+GensokyoMegaBLACKLABEL@users.noreply.github.com>
2021-01-10 18:05:30 +0700

    Update repo.js

--- repo.js
+++ repo.js
@@ -1,7 +1,9 @@
 {
 	"contact": "AutoThaiLand#6152",
 	"id": "Gensokyo-Mega-Executors",
-	"patches": {},
+	"patches":  {
+		"THSMBL_01": "Touhou Black Label 01 - Desire.EXE Rank: Extreme Mode"
+	},
 	"servers": [
 		"https://raw.githubusercontent.com/GensokyoMegaBLACKLABEL/Gensokyo-Mega-Executors/master/"
 	],
